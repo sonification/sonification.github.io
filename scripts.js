86cb0c0ade0d3914f9e58aa5c9292308905d2da7
sonification <2891264+sonification@users.noreply.github.com>
2024-11-07 16:15:17 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,11 @@
 document.querySelectorAll('g').forEach(area => {
     area.addEventListener('mouseover', () => {
         document.querySelector('.redTextImage').style.opacity = '0';
+        document.body.style.backgroundColor = 'rgb(213, 36, 30)';
     });
 
     area.addEventListener('mouseout', () => {
         document.querySelector('.redTextImage').style.opacity = '1';
+        document.body.style.backgroundColor = 'black';
     });
 });
-
